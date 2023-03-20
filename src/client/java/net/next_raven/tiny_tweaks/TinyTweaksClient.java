1b6c18e96476202ab9a37a14e7e04409d75439a8
NextRaven10 <casparsontao@gmx.de>
2023-03-20 20:01:14 +0100

    First Commit

---
+++ src/client/java/net/next_raven/tiny_tweaks/TinyTweaksClient.java
@@ -0,0 +1,10 @@
+package net.next_raven.tiny_tweaks;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TinyTweaksClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/main/java/net/next_raven/tiny_tweaks/TinyTweaksMain.java
@@ -0,0 +1,16 @@
+package net.next_raven.tiny_tweaks;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TinyTweaksMain implements ModInitializer {
+	public static final String MOD_ID = "tiny-tweaks";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
